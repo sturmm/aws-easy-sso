f3422e2989fb876be84ec8322fcefb4c77f91335
Martin Sturm <martin.sturm@rio.cloud>
2023-04-14 00:00:21 +0200

    fix: remove exit of sourced script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/false
 
 AWS_EASY_SSO_SOURCING_MODE=true aws-easy-sso-cli $@
-test $? -eq 0 && . ~/.awseasysso/export_profile || exit 1;
+test $? -eq 0 && . ~/.awseasysso/export_profile || echo "aws-easy-sso-cli failed";
